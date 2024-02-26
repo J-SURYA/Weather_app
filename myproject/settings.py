e63fb6b5424c562134deae9a0124b1ac727c5feb
SURYA J <suryajlatha52@gmail.com>
2024-02-26 23:40:14 +0530

    pre-final

--- myproject/settings.py
+++ myproject/settings.py
@@ -117,8 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
